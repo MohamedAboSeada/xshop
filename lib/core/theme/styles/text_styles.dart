155b42095b524a9bb5de1b911469c6ba351340cf
Mohamed Abo Seada <120672803+MohamedAboSeada@users.noreply.github.com>
2026-07-20 20:37:23 +0300

    chore: organize theme folder into tokens and styles.
    - create global text styles to apply through the app.
    - split the theme into styles and tokens for better organization.

---
+++ lib/core/theme/styles/text_styles.dart
@@ -0,0 +1,54 @@
+import 'package:mix/mix.dart';
+import '../tokens/design_tokens.dart';
+
+/// Variants
+final muted = NamedVariant('muted');
+final moreMuted = NamedVariant('more_muted');
+
+final medium = NamedVariant('medium');
+final semibold = NamedVariant('semibold');
+
+/// Heading styles
+final largeHeading = TextStyler().style($h1.mix()).color($primary900());
+final semiLargeHeading = TextStyler().style($h2.mix()).color($primary900());
+final mediumHeading = TextStyler().style($h3.mix()).color($primary900());
+final smallHeading = TextStyler().style($h4.mix()).color($primary900());
+
+/// Body styles
+final largeBody = TextStyler()
+    .style($b1Regular.mix())
+    .color($primary900())
+    .variants([
+      VariantStyle(medium, TextStyler().style($b1Medium.mix())),
+      VariantStyle(semibold, TextStyler().style($b1Semibold.mix())),
+      VariantStyle(muted, TextStyler().color($primary500())),
+      VariantStyle(moreMuted, TextStyler().color($primary400())),
+    ]);
+
+final mediumBody = TextStyler()
+    .style($b2Regular.mix())
+    .color($primary900())
+    .variants([
+      VariantStyle(medium, TextStyler().style($b2Medium.mix())),
+      VariantStyle(semibold, TextStyler().style($b2Semibold.mix())),
+      VariantStyle(muted, TextStyler().color($primary500())),
+      VariantStyle(moreMuted, TextStyler().color($primary400())),
+    ]);
+
+final smallBody = TextStyler()
+    .style($b3Regular.mix())
+    .color($primary900())
+    .variants([
+      VariantStyle(medium, TextStyler().style($b3Medium.mix())),
+      VariantStyle(semibold, TextStyler().style($b3Semibold.mix())),
+      VariantStyle(muted, TextStyler().color($primary500())),
+      VariantStyle(moreMuted, TextStyler().color($primary400())),
+    ]);
+
+/// Underlined link
+final linkTextStyle = TextStyler()
+    .style($b1Medium.mix())
+    .color($primary900())
+    .decoration(.underline)
+    .decorationColor($primary900())
+    .decorationThickness(1.0);

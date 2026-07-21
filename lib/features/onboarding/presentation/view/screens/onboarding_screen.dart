0d9a3a571e6ace7aea5bcd9cab3eeda4e1b09ecf
Mohamed Abo Seada <120672803+MohamedAboSeada@users.noreply.github.com>
2026-07-21 10:22:12 +0300

    feat(onboarding screen): create onboarding screen.
    - just dump interface don't do anything special.

---
+++ lib/features/onboarding/presentation/view/screens/onboarding_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:xshop/core/constant/app_assets.dart';
+import 'package:xshop/features/onboarding/presentation/view/widgets/action_button_app_bar.dart';
+import '../../../../../core/theme/tokens/design_tokens.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    /// use annotated region in order to return status bar
+    /// color to dark after changing it in splash screen.
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: .dark,
+        statusBarBrightness: .light,
+      ),
+      child: Scaffold(
+        backgroundColor: $primary0.resolve(context),
+        body: SafeArea(
+          child: Stack(
+            fit: .expand,
+            children: [
+              /// pattern behind figure
+              Positioned.fill(
+                child: SvgPicture.asset(
+                  fit: .cover,
+                  AppAssets.onboardingPatternLight,
+                ),
+              ),
+
+              /// slogan image
+              Positioned(
+                top: $space16.resolve(context).h,
+                left: $space24.resolve(context).w,
+                child: SvgPicture.asset(AppAssets.onboardingText),
+              ),
+
+              /// figure image
+              Positioned(
+                left: $space0.resolve(context),
+                right: $space0.resolve(context),
+                bottom:
+                    -MediaQuery.paddingOf(context).bottom.h -
+                    kBottomNavigationBarHeight.h -
+                    $space12.resolve(context).h,
+                child: Image.asset(fit: .cover, AppAssets.figure),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: const ActionButtonAppBar(),
+      ),
+    );
+  }
+}
